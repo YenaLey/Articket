bb384f5b24cf39f50d86e79fc017e7dd6887d4da
YenaLey <yena.e121@gmail.com>
2024-09-22 15:44:50 +0900

    Feat: 이미지를 base64로 인코딩하여 클라이언트로 전송

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,8 @@ import base64
 import shutil
 from flasgger import Swagger
 from dotenv import load_dotenv
-import uuid
+from datetime import datetime
+import pytz
 
 load_dotenv()
 
@@ -16,6 +17,9 @@ app = Flask(__name__)
 swagger = Swagger(app, template_file='./static/swagger.json')
 
 WEBUI_URL = os.getenv('WEBUI_URL')
+
+# 한국 시간대 설정 (Asia/Seoul)
+KST = pytz.timezone('Asia/Seoul')
 
 UPLOAD_FOLDER = './static/uploads'
 GENERATED_FOLDER = './static/generated'
@@ -35,35 +39,35 @@ def encode_image_to_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode('utf-8')
 
-# 보관함에 고유 이름으로 이미지 저장
-def save_to_archive(image_path, group_id, image_type, count):
-    filename = f"{group_id}_{image_type}_{count}.png"
+# 보관함에 이미지 저장
+def save_to_archive(image_path, image_type, count):
+    timestamp = datetime.now(KST).strftime("%Y%m%d_%H%M%S")
+    
+    filename = f"{timestamp}_{image_type}_{count}.png"
     archive_path = os.path.join(app.config['ARCHIVE_FOLDER'], filename)
+    
     shutil.copy(image_path, archive_path)
+    
     return archive_path
 
-# 이미지 업로드 (QR 또는 직접 촬영)
+# 이미지 업로드
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
     if 'image' not in request.files:
         return jsonify({"error": "No image file found"}), 400
     file = request.files['image']
-    filename = secure_filename(file.filename)
+
+    fixed_filename = f"uploaded_image_{1}.png"
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], fixed_filename)
     
-    # 고유한 그룹 ID 생성
-    group_id = str(uuid.uuid4())
-    
-    # 업로드 파일을 uploads 폴더에 저장
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(file_path)
     
-    # 보관함 폴더에도 업로드 파일 저장
-    save_to_archive(file_path, group_id, 'uploaded', 1)
+    save_to_archive(file_path, 'uploaded', 1)
     
     selected_artists['image_path'] = file_path
-    selected_artists['group_id'] = group_id  # 그룹 ID 저장
     
     return jsonify({"status": "image uploaded successfully", "image_path": file_path}), 200
+
 
 # CLIP으로 이미지를 텍스트로 변환하는 함수
 def clip_interrogate(image_path):
@@ -78,37 +82,33 @@ def clip_interrogate(image_path):
         return None
     return response.json()['caption']
 
-# 기능1: 내 사진을 화가 스타일로 바꿔주는 API (인물과 풍경)
-@app.route('/generate-style-images/<style>', methods=['POST'])
+# 이미지 변환
+@app.route('/generate-images/<style>', methods=['POST'])
 def generate_style_images(style):
     image_path = selected_artists.get('image_path')
-    group_id = selected_artists.get('group_id')  # 업로드된 이미지의 그룹 ID 가져오기
 
-    if not image_path or not group_id:
-        return jsonify({"error": "Missing image or group ID"}), 400
+    if not image_path:
+        return jsonify({"error": "Missing image"}), 400
 
-    # CLIP으로 이미지 텍스트 변환
     prompt = clip_interrogate(image_path)
     if not prompt:
         return jsonify({"error": "Failed to interrogate image"}), 500
 
-    # 인물과 풍경에 따른 수식어 리스트
-    if style == 'portrait':
-        modifiers = ['oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,']
-    elif style == 'landscape':
-        modifiers = ['oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,']
+    if style == 'portrait': # 인물
+        modifiers = ['oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality']
+    elif style == 'landscape': # 풍경
+        modifiers = ['oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality']
     else:
         return jsonify({"error": "Invalid style"}), 400
 
     generated_images = []
     image_base64 = encode_image_to_base64(image_path)
 
-    image_count = 1  # 생성 이미지의 번호
+    image_count = 1
 
     for modifier in modifiers:
         full_prompt = f"{modifier}, {prompt}"
 
-        # 이미지 생성 API 호출
         url = f"{WEBUI_URL}/sdapi/v1/img2img"
         headers = {"Content-Type": "application/json"}
         data = {
@@ -116,7 +116,7 @@ def generate_style_images(style):
             "prompt": full_prompt,
             "steps": 20,
             "cfg_scale": 2,
-            "denoising_strength": 0.75,
+            "denoising_strength": 0.5,
             "sampler_index": "Euler a",
             "batch_size": 1,
             "n_iter": 1
@@ -128,90 +128,46 @@ def generate_style_images(style):
 
         result = response.json()
 
-        # 생성 이미지 저장
-        image_filename = f"{image_count}.png"
+        image_filename = f"generated_image_{image_count}.png"
         generated_path = os.path.join(app.config['GENERATED_FOLDER'], image_filename)
         with open(generated_path, "wb") as f:
             f.write(base64.b64decode(result['images'][0]))
 
-        # 보관함에 이미지 저장
-        save_to_archive(generated_path, group_id, 'generated', image_count)
+        save_to_archive(generated_path, 'generated', image_count)
 
         generated_images.append(generated_path)
         image_count += 1
 
     return jsonify({"generated_images": generated_images}), 200
 
-# 기능2: 대충 그린 그림을 해당 화가 스타일로 변환하는 API (인물과 풍경)
-@app.route('/generate-enhanced-images/<style>', methods=['POST'])
-def generate_enhanced_images(style):
-    user_input = request.json.get('user_input')
-    image_path = selected_artists.get('image_path')
-    group_id = selected_artists.get('group_id')  # 업로드된 이미지의 그룹 ID 가져오기
-
-    if not image_path or not user_input or not group_id:
-        return jsonify({"error": "Missing image, user input or group ID"}), 400
-
-    # CLIP으로 이미지 텍스트 변환
-    prompt = clip_interrogate(image_path)
-    if not prompt:
-        return jsonify({"error": "Failed to interrogate image"}), 500
-
-    if style == 'portrait':
-        modifiers = ['oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,']
-    elif style == 'landscape':
-        modifiers = ['oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,', 'oil painging,style of Paul Cezanne,<lora:Paul_Cezanne:1.0>,masterpiece,best quality,']
-    else:
-        return jsonify({"error": "Invalid style"}), 400
-
-    generated_images = []
-    image_base64 = encode_image_to_base64(image_path)
-
-    image_count = 1  # 생성 이미지의 번호
-
-    for modifier in modifiers:
-        full_prompt = f"{modifier}, {prompt}, {user_input}"
-
-        # 이미지 생성 API 호출
-        url = f"{WEBUI_URL}/sdapi/v1/img2img"
-        headers = {"Content-Type": "application/json"}
-        data = {
-            "init_images": [f"data:image/png;base64,{image_base64}"],
-            "prompt": full_prompt,
-            "steps": 20,
-            "cfg_scale": 2,
-            "denoising_strength": 0.75,
-            "sampler_index": "Euler a",
-            "batch_size": 1,
-            "n_iter": 1
-        }
-        
-        response = requests.post(url, json=data, headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Failed to generate images"}), 500
-
-        result = response.json()
-
-        # 생성 이미지 저장
-        image_filename = f"enhanced_{modifier.replace(' ', '_')}.png"
-        generated_path = os.path.join(app.config['GENERATED_FOLDER'], image_filename)
-        with open(generated_path, "wb") as f:
-            f.write(base64.b64decode(result['images'][0]))
-
-        # 보관함에 저장
-        save_to_archive(generated_path, group_id, 'generated', image_count)
-
-        generated_images.append(generated_path)
-        image_count += 1
-
-    return jsonify({"generated_images": generated_images}), 200
-
-# 생성된 이미지 목록을 프론트엔드로 전송
+# 생성된 이미지 및 업로드된 이미지 목록을 프론트엔드로 전송 (base64로 인코딩하여 JSON으로 전송)
 @app.route('/get-generated-images', methods=['GET'])
 def get_generated_images():
+    uploaded_files = os.listdir(app.config['UPLOAD_FOLDER'])
     image_files = os.listdir(app.config['GENERATED_FOLDER'])
-    image_paths = [os.path.join(app.config['GENERATED_FOLDER'], img) for img in image_files]
-    return jsonify({"generated_images": image_paths}), 200
+    image_data = []
+
+    for img in uploaded_files:
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], img)
+        with open(image_path, "rb") as image_file:
+            encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+            image_data.append({
+                "filename": img,
+                "image_base64": encoded_image,
+                "type": "uploaded"
+                })
+
+    for img in image_files:
+        image_path = os.path.join(app.config['GENERATED_FOLDER'], img)
+        with open(image_path, "rb") as image_file:
+            encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+            image_data.append({
+                "filename": img,
+                "image_base64": encoded_image,
+                "type": "generated"
+            })
+    
+    return jsonify({"images": image_data}), 200
 
 # 폴더 초기화 함수
 def clear_folder(folder_path):
@@ -219,9 +175,9 @@ def clear_folder(folder_path):
         file_path = os.path.join(folder_path, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)  # 파일 또는 심볼릭 링크 삭제
+                os.unlink(file_path)
             elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)  # 디렉토리 삭제
+                shutil.rmtree(file_path)
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
 
