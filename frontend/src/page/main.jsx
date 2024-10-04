2c6601c48c1c88d9f1e9f8a6ac27296e27051663
YenaLey <yena.e121@gmail.com>
2024-10-04 15:32:03 +0900

    Feat: 백엔드 HOST 환경변수 추가

--- frontend/src/page/main.jsx
+++ frontend/src/page/main.jsx
@@ -1,7 +1,7 @@
 // HomePage.js
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import '../style/main.css';
+import "../style/main.css";
 import { FaArrowRight } from "react-icons/fa6";
 import QR from "./qr";
 
@@ -16,30 +16,29 @@ function Main() {
 
   return (
     <div className="main-container">
-
       <div className="main-title">
         <p className="main-title-top">일상 속 사진을 명작으로_아트픽스</p>
         <h4 className="main-title-title">ARTPICS</h4>
-        <p className="main-title-bottom">여러분의 사진을 화가의 손길을 거쳐 새롭게 만나보세요</p>
+        <p className="main-title-bottom">
+          여러분의 사진을 화가의 손길을 거쳐 새롭게 만나보세요
+        </p>
       </div>
 
       <div className="main-qr">
         {ipAddress && (
           <>
-            <QR pathname='upload' />
-            <p onClick={() => navigate('/upload')}>QR코드를 인식해서 사진을 선택해주세요</p>
+            <QR pathname="upload" />
+            <p onClick={() => navigate("/upload")}>
+              QR코드를 인식해서 사진을 선택해주세요
+            </p>
           </>
         )}
       </div>
       <div className="main-next">
-        <p
-          className="main-next"
-          onClick={() => navigate('/result')}
-        >
+        <p className="main-next" onClick={() => navigate("/result")}>
           <FaArrowRight /> 이미지 업로드 완료
         </p>
       </div>
-
     </div>
   );
 }
