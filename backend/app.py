e13f35632035da90c6c55e853ce111fafde1f7d6
YenaLey <yena.e121@gmail.com>
2024-10-08 13:47:36 +0900

    Feat: ARTISTS 딕셔너리로 화가 정보 통합

--- backend/app.py
+++ backend/app.py
@@ -18,31 +18,49 @@ CORS(app)
 def index():
     return 'CORS 설정 완료'
 
+swagger = Swagger(app, template_file='./static/swagger.json')
+
 REACT_APP_HOST = os.getenv('REACT_APP_HOST')
 PORT = 5000
 backend_url = f"http://{REACT_APP_HOST}:{PORT}"
-
 WEBUI_URL = os.getenv('WEBUI_URL')
+
 DESKTOP_FOLDER = os.getenv('DESKTOP_FOLDER', '../archive')  # DESKTOP_FOLDER 환경 변수에서 경로를 가져오고 없을 경우 기본 경로 설정
 UPLOAD_FOLDER = './static/uploads'
-
-swagger = Swagger(app, template_file='./static/swagger.json')
-
-if not os.path.exists(DESKTOP_FOLDER):
-    os.makedirs(DESKTOP_FOLDER)
-
-UPLOAD_FOLDER = './static/uploads'
-GENERATED_FOLDER = './static/generated'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+GENERATED_FOLDER = './static/generated'
 app.config['GENERATED_FOLDER'] = GENERATED_FOLDER
 
-for folder in [UPLOAD_FOLDER, GENERATED_FOLDER]:
+for folder in [UPLOAD_FOLDER, GENERATED_FOLDER, DESKTOP_FOLDER]:
     if not os.path.exists(folder):
         os.makedirs(folder)
 
 selected_artists = {}
 user_name = ''
 result_artist = ''
+
+ARTISTS = {
+    '고흐': {
+        'description': '창의적이고 감정적으로 풍부한 고흐',
+        'modifier': 'painting, style of Van Gogh,<lora:van_gogh:1>, masterpiece, best quality',
+        'condition': lambda a, b: a > 4
+    },
+    '피카소': {
+        'description': '강력하고 리더십이 있는 피카소',
+        'modifier': 'illustration,style of Pablo Picasso,<lora:picasso:1>,masterpiece,best quality, portrait',
+        'condition': lambda a, b: b > 4
+    },
+    '모네': {
+        'description': '안정적이고 배려 깊은 모네',
+        'modifier': 'painting, style of Claude Monet,<lora:monet:1>, masterpiece, best quality',
+        'condition': lambda a, b: a == 4 and b == 2 or a == 3 and b == 3
+    },
+    '폴록': {
+        'description': '역동적이고 즉흥적인 폴록',
+        'modifier': 'oil painging,style of Auguste Renoir, <lora:renoir:1>,masterpiece,best quality, portrait',
+        'condition': lambda a, b: a == 2 and b == 4
+    }
+}
 
 # 특정 폴더 내의 파일들을 삭제하는 함수
 def clear_folder(folder_path):
@@ -70,53 +88,6 @@ def save_to_desktop(image_path, filename):
     shutil.copy(image_path, desktop_path)
     return desktop_path
 
-### 이미지 업로드
-@app.route('/upload-image/<name>', methods=['POST'])
-def upload_image(name):
-    global user_name, count
-    if not name:
-        return jsonify({"error": "Missing user name"}), 400  # user_name 없을 때 에러 반환
-    user_name = name 
-    if 'image' not in request.files:
-        return jsonify({"error": "No image file found"}), 400
-    file = request.files['image']
-
-    clear_folder(UPLOAD_FOLDER)
-    clear_folder(GENERATED_FOLDER)
-
-    count = get_latest_count_from_desktop()
-
-    fixed_filename = f"{count}_{user_name}.png"
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], fixed_filename)
-    
-    file.save(file_path)
-    save_to_desktop(file_path, fixed_filename)
-    
-    selected_artists['image_path'] = file_path
-    selected_artists['count'] = count
-
-    file_path_for_url = backend_url + '/' + file_path.replace('./', '')
-    
-    return jsonify({"status": "image uploaded successfully", "image_path": file_path_for_url}), 200
-
-### 성격 테스트 결과를 저장
-@app.route('/get-personality-result/<options>', methods=['POST'])
-def test_result(options):
-    global result_artist
-    if not options:
-        return jsonify({"error": "Missing options"}), 400  # options가 없을 때 에러 반환
-
-    if options.count('A') >= 4:
-        result_artist = '고흐'
-    elif options.count('B') >= 4:
-        result_artist = '피카소'
-    elif options.count('A') >= 3 and options.count('B') <= 2:
-        result_artist = '모네'
-    else:
-        result_artist = '폴록'
-
-    return jsonify({"artist": result_artist}), 200
-
 # 이미지 파일을 base64로 인코딩하는 함수
 def encode_image_to_base64(image_path):
     with open(image_path, "rb") as img_file:
@@ -140,13 +111,76 @@ def clip_interrogate(image_path, clip_skip_level=1):
         return None
     return response.json().get('caption', '')
 
-### 이미지 생성 및 이미지 경로 전송
+'''
+이미지 업로드 API
+사용자가 이미지를 업로드하고, 서버에 저장된 이미지의 경로를 반환합니다.
+'''
+@app.route('/upload-image/<name>', methods=['POST'])
+def upload_image(name):
+    global user_name, count
+    if not name:
+        return jsonify({"error": "Missing user name"}), 400
+    user_name = name 
+    if 'image' not in request.files:
+        return jsonify({"error": "No image file found"}), 400
+    file = request.files['image']
+
+    clear_folder(UPLOAD_FOLDER)
+    clear_folder(GENERATED_FOLDER)
+
+    count = get_latest_count_from_desktop()
+
+    fixed_filename = f"{count}_{user_name}_original.png"
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], fixed_filename)
+    
+    file.save(file_path)
+    save_to_desktop(file_path, fixed_filename)
+    
+    selected_artists['image_path'] = file_path
+    selected_artists['count'] = count
+
+    file_path_for_url = backend_url + '/' + file_path.replace('./', '')
+    
+    return jsonify({"status": "image uploaded successfully", "image_path": file_path_for_url}), 200
+
+'''
+성격 테스트 결과 전송 API
+사용자의 선택을 바탕으로 성격에 해당되는 화가의 이름을 반환합니다.
+'''
+@app.route('/get-personality-result/<options>', methods=['POST'])
+def test_result(options):
+    global result_artist
+    if not options:
+        return jsonify({"error": "Missing options"}), 400
+
+    a_count = options.count('A')
+    b_count = options.count('B')
+
+    for artist_name, artist_info in ARTISTS.items():
+        condition = artist_info['condition']
+        if condition(a_count, b_count):
+            result_artist = artist_name
+            break
+
+    if not result_artist:
+        return jsonify({"error": "No matching artist found"}), 400
+
+    return jsonify({"artist": result_artist}), 200
+
+'''
+이미지 생성 및 이미지 경로 전송 API
+사용자가 업로드한 이미지를 지정된 화가 스타일로 변환하고 경로를 반환합니다.
+'''
 @app.route('/get-generated-images', methods=['POST'])
 async def generate_style_images():
     global count, result_artist
 
-    if not result_artist:
-        return jsonify({"error": "Missing result artist"}), 400  # result_artist가 없을 때 에러 반환
+    artist_info = ARTISTS.get(result_artist)
+    if not artist_info:
+        return jsonify({"error": "Missing result artist"}), 400
+
+    adorned_artist = artist_info['description']
+    modifier = artist_info['modifier']
 
     image_path = selected_artists.get('image_path')
 
@@ -157,18 +191,9 @@ async def generate_style_images():
     if not prompt:
         return jsonify({"error": "Failed to interrogate image"}), 500
 
-    modifiers = [
-        ('painting, style of Van Gogh,<lora:van_gogh:1>, masterpiece, best quality', '고흐'),
-        ('illustration,style of Pablo Picasso,<lora:picasso:1>,masterpiece,best quality, portrait', '피카소'), 
-        ('painting, style of Claude Monet,<lora:monet:1>, masterpiece, best quality', '모네'),
-        ('oil painging,style of Auguste Renoir, <lora:renoir:1>,masterpiece,best quality, portrait', '폴록')
-    ]
-
     current_count = selected_artists.get('count')
-    generated_images = []
     image_base64 = encode_image_to_base64(image_path)
 
-    # 이미지 생성 함수
     async def generate_image(modifier):
         full_prompt = f"{modifier}, {prompt}"
         url = f"{WEBUI_URL}/sdapi/v1/img2img"
@@ -205,14 +230,11 @@ async def generate_style_images():
 
         return file_path_for_url
 
-    # result_artist에 해당하는 스타일로만 이미지 생성
-    tasks = [generate_image(modifier) for modifier, artist in modifiers if artist == result_artist]
-    generated_images = await asyncio.gather(*tasks)
+    generated_image_url = await generate_image(modifier)
 
     count += 1
 
-    return jsonify({"user_name": user_name, "artist": result_artist, "generated_images": generated_images}), 200
-
+    return jsonify({"user_name": user_name, "artist": adorned_artist, "generated_image": generated_image_url}), 200
 
 if __name__ == '__main__':
     try:
