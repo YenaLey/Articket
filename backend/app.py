3dc6d7a6999cf6c2577543dde2e4c2f45a22daa6
YenaLey <yena.e121@gmail.com>
2024-11-11 23:30:48 +0900

    Feat: 백엔드 웹소켓 추가

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
+from flask_socketio import SocketIO
 import os
 import shutil
 import base64
 import requests
 import time
-import asyncio
 from flasgger import Swagger
 from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 load_dotenv(dotenv_path='../frontend/.env')
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
@@ -23,7 +25,12 @@ swagger = Swagger(app, template_file='./static/swagger.json')
 REACT_APP_HOST = os.getenv('HOST')
 PORT = 5000
 backend_url = f"http://{REACT_APP_HOST}:{PORT}"
+
+# Stable Diffusion WebUI URL 설정
 WEBUI_URL = os.getenv('WEBUI_URL')
+
+# BLIP API URL 설정 (환경 변수에서 가져옴)
+BLIP_URL = os.getenv('BLIP_URL')
 
 DESKTOP_FOLDER = os.getenv('DESKTOP_FOLDER', '../archive')  # DESKTOP_FOLDER 환경 변수에서 경로를 가져오고 없을 경우 기본 경로 설정
 UPLOAD_FOLDER = './static/uploads'
@@ -44,96 +51,68 @@ upload_status = {
 }
 
 def calculate_mbti(options_list):
-    E, I = 0, 0
-    N, S = 0, 0
-    T, F = 0, 0
-    J, P = 0, 0
+    E, I, N, S, T, F, J, P = 0, 0, 0, 0, 0, 0, 0, 0
+    # 조건을 통한 성향 카운트
+    E, I = (E + 1, I) if options_list[0].upper() == 'A' else (E, I + 1)
+    E, I = (E + 1, I) if options_list[6].upper() == 'A' else (E, I + 1)
+    N, S = (N + 1, S) if options_list[2].upper() == 'A' else (N, S + 1)
+    N, S = (N + 1, S) if options_list[3].upper() == 'A' else (N, S + 1)
+    T, F = (T + 1, F) if options_list[4].upper() == 'A' else (T, F + 1)
+    T, F = (T + 1, F) if options_list[7].upper() == 'A' else (T, F + 1)
+    J, P = (J + 1, P) if options_list[1].upper() == 'A' else (J, P + 1)
+    J, P = (J + 1, P) if options_list[5].upper() == 'A' else (J, P + 1)
 
-    # E/I
-    if options_list[0].upper() == 'A':
-        E += 1
-    else:
-        I += 1
-    if options_list[4].upper() == 'A':
-        E += 1
-    else:
-        I += 1
-
-    # N/S
-    if options_list[1].upper() == 'A':
-        N += 1
-    else:
-        S += 1
-    if options_list[5].upper() == 'A':
-        N += 1
-    else:
-        S += 1
-
-    # T/F
-    if options_list[2].upper() == 'A':
-        T += 1
-    else:
-        F += 1
-    if options_list[6].upper() == 'A':
-        T += 1
-    else:
-        F += 1
-
-    # J/P
-    if options_list[3].upper() == 'A':
-        J += 1
-    else:
-        P += 1
-    if options_list[7].upper() == 'A':
-        J += 1
-    else:
-        P += 1
-
-    mbti = ''
-    mbti += 'E' if E >= I else 'I'
-    mbti += 'N' if N >= S else 'S'
-    mbti += 'T' if T >= F else 'F'
-    mbti += 'J' if J >= P else 'P'
-
-    return mbti
+    return ''.join([
+        'E' if E >= I else 'I',
+        'N' if N >= S else 'S',
+        'T' if T >= F else 'F',
+        'J' if J >= P else 'P'
+    ])
 
 ARTISTS = {
     '리히텐슈타인': {
         'description': '세련된 일상의 리히텐슈타인',
-        'modifier': 'pop art, <lora:loy_xl-000013:1>, masterpiece,best quality, background with a dotted halftone pattern,portrait, ',
-        'negative_prompt': 'lowres,bad anatomy,bad hands,text,error,missing fingers,extra digit,fewer digits,cropped,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,username,blurry,nsfw,',
+        'modifier': 'pop art, <lora:loy_xl-000013:1>, masterpiece, best quality, background with a dotted halftone pattern, portrait,',
+        'negative_prompt': 'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, nsfw,',
         'steps': 150,
         'denoising_strength': 0.73,
         'cfg_scale': 7,
-        'condition': lambda options_list: calculate_mbti(options_list) in ["ENFP", "ENTP", "ESFP", "ESTP"]
+        'condition': lambda options_list: calculate_mbti(options_list) in ["ENFP", "ESFP", "ESTP", "ENTP"]
     },
     '고흐': {
         'description': '감정과 열정의 섬세한 고흐',
-        'modifier': 'painging, <lora:gogh_xl-000011:1>,masterpiece,best quality,Starry Night, portrait,',
-        'negative_prompt': 'lowres,bad anatomy,bad hands,text,error,missing fingers,extra digit,fewer digits,cropped,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,username,blurry,nsfw,yellow face,',
+        'modifier': 'painting, <lora:gogh_xl-000011:1>, masterpiece, best quality, Starry Night, portrait,',
+        'negative_prompt': 'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, nsfw, yellow face,',
         'steps': 100,
         'denoising_strength': 0.75,
         'cfg_scale': 7,
-        'condition': lambda options_list: calculate_mbti(options_list) in ["INTJ", "INFJ", "ISTJ", "ISFJ"]
+        'condition': lambda options_list: calculate_mbti(options_list) in ["INFJ", "INFP", "INTJ", "INTP"]
     },
     '피카소': {
         'description': '대담하고 창의적인 피카소',
-        'modifier': 'illustration,style of Pablo Picasso, <lora:picasso_xl-000008:1>, masterpiece,best quality, portrait,',
-        'negative_prompt': 'lowres,bad anatomy,bad hands,text,error,missing fingers,extra digit,fewer digits,cropped,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,username,blurry,nsfw,',
+        'modifier': 'illustration, style of Pablo Picasso, <lora:picasso_xl-000008:1>, masterpiece, best quality, portrait,',
+        'negative_prompt': 'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, nsfw,',
         'steps': 100,
         'denoising_strength': 0.75,
         'cfg_scale': 7,
-        'condition': lambda options_list: calculate_mbti(options_list) in ["ENTJ", "ENFJ", "ESTJ", "ESFJ"]
+        'condition': lambda options_list: calculate_mbti(options_list) in ["ENTJ", "ISTJ", "ESTJ", "ISTP"]
     },
     '르누아르': {
         'description': '낙천적이고 따뜻한 르누아르',
-        'modifier': 'oil painging,style of Auguste Renoir, <lora:renoir_70_40_4:1>,masterpiece,best quality, portrait,',
-        'negative_prompt': 'lowres,bad anatomy,bad hands,text,error,missing fingers,extra digit,fewer digits,cropped,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,username,blurry,nsfw,',
+        'modifier': 'oil painting, style of Auguste Renoir, <lora:renoir_70_40_4:1>, masterpiece, best quality, portrait,',
+        'negative_prompt': 'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, nsfw,',
         'steps': 100,
         'denoising_strength': 0.73,
         'cfg_scale': 7,
-        'condition': lambda options_list: calculate_mbti(options_list) in ["INFP", "INTP", "ISFP", "ISTP"]
+        'condition': lambda options_list: calculate_mbti(options_list) in ["ISFJ", "ESFJ", "ISFP", "ENFJ"]
     }
+}
+
+MATCHING_ARTISTS = {
+    '피카소': {'good': '르누아르', 'bad': '리히텐슈타인'},
+    '고흐': {'good': '리히텐슈타인', 'bad': '피카소'},
+    '르누아르': {'good': '피카소', 'bad': '고흐'},
+    '리히텐슈타인': {'good': '고흐', 'bad': '르누아르'},
 }
 
 # 특정 폴더 내의 파일들을 삭제하는 함수
@@ -167,23 +146,40 @@ def encode_image_to_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode('utf-8')
 
-# CLIP으로 이미지를 텍스트로 변환하는 함수
-def clip_interrogate(image_path, clip_skip_level=1):
-    start_time = time.time()
+# BLIP으로 이미지를 텍스트로 변환하는 함수
+def blip_interrogate(image_path):
     image_base64 = encode_image_to_base64(image_path)
-    interrogate_url = f"{WEBUI_URL}/sdapi/v1/interrogate"
-    interrogate_data = {
-        "image": f"data:image/png;base64,{image_base64}",
-        "model": "clip",
-        "clip_skip": clip_skip_level
-    }
-    response = requests.post(interrogate_url, json=interrogate_data)
-    elapsed_time = time.time() - start_time
-    print(f"CLIP 소요 시간: {elapsed_time:.2f} 초")
 
-    if response.status_code != 200:
-        return None
-    return response.json().get('caption', '')
+    # interrogate_url = f"{BLIP_URL}/api/v1/blip"
+    # interrogate_data = {"image": f"data:image/png;base64,{image_base64}"}
+
+    interrogate_url = f"{WEBUI_URL}/sdapi/v1/interrogate"
+    interrogate_data = {"image": f"data:image/png;base64,{image_base64}", "model": "clip", "clip_skip": 1}
+
+    response = requests.post(interrogate_url, json=interrogate_data, headers={"Content-Type": "application/json"})
+    return response.json().get('caption', '') if response.status_code == 200 else None
+
+def generate_image(image_base64, modifier, negative_prompt, steps, denoising_strength, cfg_scale, prompt, result_number):
+    current_count = get_latest_count_from_desktop()
+    data = {
+        "init_images": [f"data:image/png;base64, {image_base64}"],
+        "prompt": f"{modifier}, {prompt}",
+        "negative_prompt": negative_prompt,
+        "steps": steps,
+        "cfg_scale": cfg_scale,
+        "denoising_strength": denoising_strength,
+        "sampler_index": "Euler a",
+        "batch_size": 1,
+        "n_iter": 1
+    }
+    response = requests.post(f"{WEBUI_URL}/sdapi/v1/img2img", json=data, headers={"Content-Type": "application/json"})
+    if response.status_code == 200:
+        image_filename = f"{current_count}_{user_name}_result{result_number}.png"
+        generated_path = os.path.join(app.config['GENERATED_FOLDER'], image_filename)
+        with open(generated_path, "wb") as f:
+            f.write(base64.b64decode(response.json()['images'][0]))
+        save_to_desktop(generated_path, image_filename)
+        return backend_url + '/' + generated_path.replace('./', '')
 
 '''
 이미지 업로드 API
@@ -191,47 +187,31 @@ def clip_interrogate(image_path, clip_skip_level=1):
 '''
 @app.route('/upload-image/<name>', methods=['POST'])
 def upload_image(name):
-    global user_name, count, upload_status
-    if not name:
-        return jsonify({"error": "Missing user name"}), 400
-    user_name = name 
-    if 'image' not in request.files:
-        return jsonify({"error": "No image file found"}), 400
-    
-    # 업로드 상태 초기화
-    upload_status['status'] = 'uploading'
-    upload_status['message'] = '이미지 업로드 중...'
-
+    global user_name
+    if not name or 'image' not in request.files:
+        return jsonify({"error": "Missing user name or image file"}), 400
+    user_name = name
     file = request.files['image']
-
     clear_folder(UPLOAD_FOLDER)
     clear_folder(GENERATED_FOLDER)
-
     count = get_latest_count_from_desktop()
-
-    fixed_filename = f"{count}_{user_name}_original.png"
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], fixed_filename)
-    
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{count}_{user_name}_original.png")
     file.save(file_path)
-    save_to_desktop(file_path, fixed_filename)
-    
+    save_to_desktop(file_path, f"{count}_{user_name}_original.png")
     selected_artists['image_path'] = file_path
     selected_artists['count'] = count
+    upload_status.update({'status': 'completed', 'message': '이미지 업로드 완료'})
+    socketio.emit('operation_status', {'success': True})
+    return jsonify({"image_path": backend_url + '/' + file_path.replace('./', '')}), 200
 
-    file_path_for_url = backend_url + '/' + file_path.replace('./', '')
-
-    # 업로드 완료 상태로 변경
-    upload_status['status'] = 'completed'
-    upload_status['message'] = '이미지 업로드 완료'
-    
-    return jsonify({"image_path": file_path_for_url}), 200
 
 '''
-클라이언트에서 이미지 업로드 상태를 확인하는 API.
+성격 테스트에서 옵션을 선택할 때 호출하는 API.
 '''
-@app.route('/upload-image-status', methods=['GET'])
-def get_upload_status():
-    return jsonify(upload_status), 200
+@app.route('/select-option', methods=['GET'])
+def select_option():
+    socketio.emit('operation_status', {'success': True})
+    return 200
 
 '''
 성격 테스트 결과 전송 API
@@ -240,108 +220,42 @@ def get_upload_status():
 @app.route('/get-personality-result/<options>', methods=['POST'])
 def test_result(options):
     global result_artist
-    if not options:
-        return jsonify({"error": "Missing options"}), 400
-
-    options_list = list(options.upper())
-    if len(options_list) != 8:
+    if len(options) != 8:
         return jsonify({"error": "Invalid number of options"}), 400
-
-    mbti = calculate_mbti(options_list)
-    print(f"Calculated MBTI: {mbti}")
-
-    matched_artists = []
-
-    for artist_name, artist_info in ARTISTS.items():
-        condition = artist_info['condition']
-        if condition(options_list):
-            matched_artists.append(artist_name)
-
-    if not matched_artists:
+    result_artist = next((artist for artist, info in ARTISTS.items() if info['condition'](list(options.upper()))), None)
+    
+    if result_artist:
+        socketio.emit('operation_status', {'success': True})
+        return jsonify({"artist": result_artist, "mbti": calculate_mbti(list(options.upper()))}), 200
+    else:
         return jsonify({"error": "No matching artist found"}), 400
 
-    result_artist = matched_artists[0]
-
-    return jsonify({"artist": result_artist, "mbti": mbti}), 200
-
+    
 '''
-이미지 생성 및 이미지 경로 전송 API
-사용자가 업로드한 이미지를 지정된 화가 스타일로 변환하고 경로를 반환합니다.
+해당 화가의 스타일로 변환된 이미지를 생성 후 저장하는 API
 '''
 @app.route('/get-generated-images', methods=['POST'])
-async def generate_style_images():
+def generate_style_images():
     global count, result_artist
+    if not (image_path := selected_artists.get('image_path')) or not (artist_info := ARTISTS.get(result_artist)):
+        return jsonify({"error": "Missing image or result artist"}), 400
 
-    artist_info = ARTISTS.get(result_artist)
-    if not artist_info:
-        return jsonify({"error": "Missing result artist"}), 400
-
-    adorned_artist = artist_info['description']
-    modifier = artist_info['modifier']
-    negative_prompt = artist_info.get('negative_prompt')
-    steps = artist_info.get('steps')
-    denoising_strength = artist_info.get('denoising_strength')
-    cfg_scale = artist_info.get('cfg_scale')
-
-    image_path = selected_artists.get('image_path')
-
-    if not image_path:
-        return jsonify({"error": "Missing image"}), 400
-
-    prompt = await asyncio.to_thread(clip_interrogate, image_path, clip_skip_level=1)
+    prompt = blip_interrogate(image_path)
+    image_base64 = encode_image_to_base64(image_path)
     if not prompt:
         return jsonify({"error": "Failed to interrogate image"}), 500
 
-    current_count = selected_artists.get('count')
-    image_base64 = encode_image_to_base64(image_path)
+    matching_artists = [result_artist, MATCHING_ARTISTS[result_artist]['good'], MATCHING_ARTISTS[result_artist]['bad']]
+    urls = []
 
-    async def generate_image(modifier, negative_prompt, steps, denoising_strength, cfg_scale, prompt):
-        full_prompt = f"{modifier}, {prompt}"
-        url = f"{WEBUI_URL}/sdapi/v1/img2img"
-        headers = {"Content-Type": "application/json"}
-        data = {
-            "init_images": [f"data:image/png;base64,{image_base64}"],
-            "prompt": full_prompt,
-            "negative_prompt": negative_prompt,
-            "steps": steps,
-            "cfg_scale": cfg_scale,
-            "denoising_strength": denoising_strength,
-            "sampler_index": "Euler a",
-            "batch_size": 1,
-            "n_iter": 1
-        }
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(generate_image, image_base64, ARTISTS[artist]['modifier'], ARTISTS[artist]['negative_prompt'], ARTISTS[artist]['steps'], ARTISTS[artist]['denoising_strength'], ARTISTS[artist]['cfg_scale'], prompt, idx + 1) for idx, artist in enumerate(matching_artists)]
+        for future in as_completed(futures):
+            urls.append(future.result())
 
-        start_time = time.time()
-        response = await asyncio.to_thread(requests.post, url, json=data, headers=headers)
-        elapsed_time = time.time() - start_time
-        print(f"이미지 생성 소요 시간 (수식어: {modifier}): {elapsed_time:.2f} 초")
+    socketio.emit('operation_status', {'success': True})
+    return jsonify({"user_name": user_name, "artist": artist_info['description'], "generated_image": urls, "qr_image": backend_url + '/static/personality-result-qr.png'}), 200
 
-        if response.status_code != 200:
-            return None
-
-        result = response.json()
-
-        image_filename = f"{current_count}_{user_name}_result.png"
-        generated_path = os.path.join(app.config['GENERATED_FOLDER'], image_filename)
-        with open(generated_path, "wb") as f:
-            f.write(base64.b64decode(result['images'][0]))
-
-        save_to_desktop(generated_path, image_filename)
-
-        file_path_for_url = backend_url + '/' + generated_path.replace('./', '')
-
-        return file_path_for_url
-
-    generated_image_url = await generate_image(modifier, negative_prompt, steps, denoising_strength, cfg_scale, prompt)
-
-    count += 1
-
-    return jsonify({
-        "user_name": user_name,
-        "artist": adorned_artist,
-        "generated_image": generated_image_url,
-        "qr_image": backend_url + '/static/personality-result-qr.png'
-    }), 200
 
 if __name__ == '__main__':
     try:
--- frontend/src/page/main.jsx
+++ frontend/src/page/main.jsx
@@ -6,13 +6,15 @@ import QR from "./qr";
 import axios from "axios";
 
 function Main() {
-  const [uploadStatus, setUploadStatus] = useState(null); 
+  const [uploadStatus, setUploadStatus] = useState(null);
   const navigate = useNavigate();
 
   // 주기적으로 상태를 확인하는 함수
   const checkUploadStatus = async () => {
     try {
-      const response = await axios.get(`http://${process.env.REACT_APP_HOST}:5000/upload-image-status`);
+      const response = await axios.get(
+        `http://${process.env.REACT_APP_HOST}:5000/upload-image-status`
+      );
       if (response.status === 200) {
         setUploadStatus(response.data);
       }
@@ -21,20 +23,20 @@ function Main() {
     }
   };
 
-    // 상태가 변경되었을 때, 'completed' 상태로 변경되면 navigate 실행
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        checkUploadStatus();
-      }, 10000); // 10초마다 상태 확인
-  
-      return () => clearInterval(intervalId); // 컴포넌트가 unmount 될 때, interval 정리
-    }, []);
-  
-    useEffect(() => {
-      if (uploadStatus && uploadStatus.status === 'completed') {
-        navigate('/test'); // 'completed' 상태일 때 페이지 이동
-      }
-    }, [uploadStatus, navigate]);
+  // 상태가 변경되었을 때, 'completed' 상태로 변경되면 navigate 실행
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      checkUploadStatus();
+    }, 10000); // 10초마다 상태 확인
+
+    return () => clearInterval(intervalId); // 컴포넌트가 unmount 될 때, interval 정리
+  }, []);
+
+  useEffect(() => {
+    if (uploadStatus && uploadStatus.status === "completed") {
+      navigate("/test"); // 'completed' 상태일 때 페이지 이동
+    }
+  }, [uploadStatus, navigate]);
 
   return (
     <div className="main-container">
@@ -56,7 +58,9 @@ function Main() {
         <p onClick={() => navigate("/test")}>나의 예술가 유형 테스트하기 </p>
       </div>
       <div className="main-upload">
-        <p>이미지 업로드 상태: {uploadStatus ? uploadStatus.status : '대기 중'}</p>
+        <p>
+          이미지 업로드 상태: {uploadStatus ? uploadStatus.status : "대기 중"}
+        </p>
       </div>
     </div>
   );
