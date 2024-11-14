31d1531b5b6d449428fd5163b599336b9d19de12
YenaLey <yena.e121@gmail.com>
2024-11-15 00:52:05 +0900

    Feat: 사진 생성 크기 지정

--- frontend/src/page/result.jsx
+++ frontend/src/page/result.jsx
@@ -18,40 +18,46 @@ export default function Result() {
     { src: "/img/피카소.png", artist: "피카소" },
     { src: "/img/리히텐슈타인.png", artist: "리히텐슈타인" },
     { src: "/img/고흐.png", artist: "고흐" },
-    { src: "/img/르누아르.png", artist: "르누아르" }
+    { src: "/img/르누아르.png", artist: "르누아르" },
   ];
 
   // 생성된 이미지와 기타 데이터 가져오기
   const getGeneratedImages = async () => {
     try {
-      const response = await fetch(`http://${process.env.REACT_APP_HOST}:5000/get-generated-images`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
+      const response = await fetch(
+        `http://${process.env.REACT_APP_HOST}:5000/get-generated-images`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
       if (!response.ok) {
-        throw new Error('Failed to fetch generated images');
+        throw new Error("Failed to fetch generated images");
       }
-  
+
       const generatedResult = await response.json();
       console.log(generatedResult);
       const { user_name, artist, generated_image, qr_image } = generatedResult;
-  
+
       setUserName(user_name);
       setArtist(artist);
-  
+
       // 배열인지 확인 후 설정
-      setGeneratedImageUrls(Array.isArray(generated_image) ? generated_image : [generated_image]);
-  
+      setGeneratedImageUrls(
+        Array.isArray(generated_image) ? generated_image : [generated_image]
+      );
+
       setQrImageUrl(qr_image);
     } catch (error) {
       console.error(error);
-      alert('An error occurred while fetching generated images. Please try again.');
+      alert(
+        "An error occurred while fetching generated images. Please try again."
+      );
     } finally {
       setLoading(false);
     }
   };
-  
 
   // 컴포넌트가 마운트될 때 API 호출
   useEffect(() => {
@@ -67,13 +73,18 @@ export default function Result() {
           <div className="loading-img-container">
             {imgSample.map((element, index) => (
               <div className="loading-img" key={index}>
-                <img src={process.env.PUBLIC_URL + element.src} alt={element.artist} />
+                <img
+                  src={process.env.PUBLIC_URL + element.src}
+                  alt={element.artist}
+                />
                 <p>{element.artist}</p>
               </div>
             ))}
           </div>
           <div className="loading-loading">
-            <p>성격 유형을 분석하여 해당 화가 스타일로 이미지를 변환 중이에요</p>
+            <p>
+              성격 유형을 분석하여 해당 화가 스타일로 이미지를 변환 중이에요
+            </p>
             <HashLoader color="#D8D8D8" size={50} />
           </div>
         </div>
@@ -96,14 +107,14 @@ export default function Result() {
             )}
           </div>
 
-          <div className="qr-container">
+          {/* <div className="qr-container">
             {qrImageUrl && (
               <div className="qr-image">
                 <img src={qrImageUrl} alt="QR code" />
                 <p>QR 코드를 스캔하여 성격 테스트 결과를 확인하세요</p>
               </div>
             )}
-          </div>
+          </div> */}
         </div>
       )}
     </div>
